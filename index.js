5addb93839ffcc440124505de99767cf3129218a
Zine Samy <zine.samy@gmail.com>
2018-05-19 00:12:12 +0200

    test

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var angular2_indexeddb_1 = require("./angular2-indexeddb");
+exports.AngularIndexedDB = angular2_indexeddb_1.AngularIndexedDB;
+//# sourceMappingURL=index.js.map
